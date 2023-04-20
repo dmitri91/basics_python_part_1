6820ce25aae816e096ad641047622d67309b4d39
Dmitrii <dalariono1@example.com>
2023-04-20 22:04:47 +0300

    added func in last test

--- basics_python_2.py
+++ basics_python_2.py
@@ -82,19 +82,22 @@ def test_readable_function():
     find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
 
 
+def print_name_function(func, *args):
+    result = func.__name__.replace("_", " ").title() + f" [{', '.join(args)}]"
+    print(result)
+    return result
+
+
 def open_browser(browser_name):
-    actual_result = open_browser.__name__.replace("_", " ").title() + f" [{browser_name}]"
-    print(actual_result)
+    actual_result = print_name_function(open_browser, browser_name)
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
-    actual_result = go_to_companyname_homepage.__name__.replace("_", " ").title() + f" [{page_url}]"
-    print(actual_result)
+    actual_result = print_name_function(go_to_companyname_homepage, page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = find_registration_button_on_login_page.__name__.replace("_", " ").title() + f" [{page_url}, {button_text}]"
-    print(actual_result)
+    actual_result = print_name_function(find_registration_button_on_login_page, page_url, button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
